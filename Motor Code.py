b696770ddaeec8857f7a0d803e97ed8f06ddf13b
Jordan <jordan.camilletti@gmail.com>
2019-08-15 21:39:31 -0500

    Changed name of a file

---
+++ Motor Code.py
@@ -0,0 +1,15 @@
+import time
+import board
+import pulseio
+import adafruit_irremote
+from digitalio import DigitalInOut, Direction
+
+motor = DigitalInOut(board.D2)
+motor.direction = Direction.OUTPUT
+motor.value = False
+while(True):
+    if(motor.value):
+        motor.value = False
+    else:
+        motor.value = True
+    time.sleep(0.5)
